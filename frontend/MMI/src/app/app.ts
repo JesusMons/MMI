1f977c9b958c8a0afbb5770ba5bb9faec8fb1ac3
Jesus Monsalvo <jesusmonsalvo2003b@gmail.com>
2025-07-22 22:45:11 -0500

    Se elimino todo desde 0

--- frontend/MMI/src/app/app.ts
+++ frontend/MMI/src/app/app.ts
@@ -1,13 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header} from './components/layout/header/header';
-import { Footer } from './components/layout/footer/footer';
-import { Content } from './components/layout/content/content';
-import { Aside } from './components/layout/aside/aside';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Content , Aside],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
